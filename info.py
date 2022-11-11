1c1c8d5fafb37545fbf6574292833a773e044a17
Ken <98230827+ItzmeKen@users.noreply.github.com>
2022-11-11 20:57:30 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,28 @@
+import sys,os
+from colorama import Fore
+print(Fore.MAGENTA+"""
+
+Xmas Attacker is bunch of tools which we made for people.These all tools are for purpose of ethical hacking and discord tools.
+
+Who is the Developer, Xmas Attack is made by ! - 𝘒 𝘌 𝘕. This tool Rights only they have if anyone would try to copy it use under there name would be affected by the MIT Licence
+
+Remember to use this tool on your own purpose no wrong use of it
+
+This is still a beta version is over now and that's the Version 0.1
+
+""")
+print(Fore.RED+"""Getting Started
+
+git clone https://github.com/ItzmeKen/Xmas-attacker.git
+cd Xmas-Attacker
+python -m pip install -r requirements.txt
+for(Windows user just run the bat file (start.bat) )
+Run Python xmas-tool.py
+
+
+License
+Xmas-Tool is under the MIT License
+Using it without giving us credit would lead to Breaking the License law
+
+
+""")
